c0b043cedffed33d442a276274a02b24814bfdab
bebari <rok.plevel@gmail.com>
2017-12-06 00:02:19 +0100

    service registration

--- api/src/main/java/com/rso/bank/accounts/api/v1/AccountApplication.java
+++ api/src/main/java/com/rso/bank/accounts/api/v1/AccountApplication.java
@@ -1,8 +1,11 @@
 package com.rso.bank.accounts.api.v1;
 
+import com.kumuluz.ee.discovery.annotations.RegisterService;
+
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
 @ApplicationPath("/v1")
+@RegisterService
 public class AccountApplication extends Application {
 }
